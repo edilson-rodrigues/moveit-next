0f45370f42a4a073a9c1ec1cc1b795cf36244e21
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-24 09:54:36 -0300

    create component profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+const Profile = () => (
+  <div>
+    <img src="https://github.com/edilson-rodrigues.png" alt="profile-github" />
+    <div>
+      <strong>Edilson Rodrigues</strong>
+      <p>Level 1</p>
+    </div>
+  </div>
+);
+
+export default Profile;
