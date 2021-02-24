d8b0eb0747e7b99d34d4007e12780eb466a31bc5
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-24 11:18:12 -0300

    component countdown init state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,14 @@ import Profile from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
 import CompletedChallenges from '../components/CompletedChallenges';
 import Countdown from '../components/Countdown';
+import Head  from 'next/Head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
